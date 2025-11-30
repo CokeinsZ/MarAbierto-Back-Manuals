a275324bd3d64d78d86fc5e1f79910b458816ec1
Stiven Carvajal <stivencarvajal205@gmail.com>
2025-11-30 02:23:47 -0500

    Add ValidationPipe and CORS to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuración de CORS
+  app.enableCors({
+    origin: '*', // Permite cualquier origen
+    methods: '*', // Permite cualquier método (GET, POST, PUT, DELETE, etc.)
+    allowedHeaders: '*', // Permite cualquier header
+    credentials: true, // Permite el envío de cookies y credenciales
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
+
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
